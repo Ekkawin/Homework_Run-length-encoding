ec83625c3f4fdb4d90b4abf324e2d6f4e0a6853f
Ekkawin <ekkawin.v@gmail.com>
2020-06-06 17:16:21 +0700

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
  This function eliminate all of the white space between any word or character.
  It also a case sensitive function.
 
+ TODO
+    Make a feature to guide the passorword for users when they forget.
+    If users click "forget password" botton, it will generate the missing characters followed by 
+    the number of the missing of those characters.
+    For example,
+    User sets the password as "Skooldio" but user forgets the password. User inputs "School" as
+    his/her password. This feature will generate "k1d1i1o1" when user clicks "forget password" button.
+    It will guide user the missing character. Eventually, user can guess their password.
+
+
 */
 
 let argument = '';
